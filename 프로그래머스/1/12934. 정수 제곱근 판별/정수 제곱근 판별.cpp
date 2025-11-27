ebd0cc361551c7fc7af675b4931db056ddcd2def
HyeonTaek Lee <76274305+dlgusxor12@users.noreply.github.com>
2025-11-27 22:22:45 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    
+    if(sqrt(n) - (long long)(sqrt(n)) == 0){
+        answer = (sqrt(n) + 1) * (sqrt(n) + 1);
+    }else{
+        answer = -1;
+    }
+    
+    
+    return answer;
+}
