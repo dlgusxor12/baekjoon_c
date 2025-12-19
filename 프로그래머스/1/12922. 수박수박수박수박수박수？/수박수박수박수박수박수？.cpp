1a738bd0da33bf505616129355d4a34f2729c7b1
HyeonTaek Lee <76274305+dlgusxor12@users.noreply.github.com>
2025-12-20 00:33:17 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.07 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    
+    if(n%2 == 1){
+        if(n==1){
+            return "수";
+        }
+        for(int i=0; i<n/2; i++){
+            answer += "수박";
+        }
+        answer += "수";
+    }
+    else{
+        for(int i=0; i<n/2; i++){
+            answer += "수박";
+        }
+    }
+    
+    return answer;
+}
