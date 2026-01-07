cb7f2a7d60ee62d19b61fc4065911f1505a7556a
HyeonTaek Lee <76274305+dlgusxor12@users.noreply.github.com>
2026-01-07 12:01:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <numeric>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    
+    answer.push_back(gcd(n, m));
+    answer.push_back(lcm(n, m));
+    
+    return answer;
+}
